8b82f27f7ac53cbfae7bb12d6c5874d99623217f
samhu1989 <hsy19891228@yeah.net>
2019-01-17 22:50:26 +0800

    delete some tasks

--- util/tasks/trainAE01.py
+++
@@ -1,5 +0,0 @@
-#
-from .task import Task;
-class RealTask(Task):
-    def __init__(self):
-        super(RealTask,self).__init__();
--- util/tasks/trainSVR01.py
+++
@@ -1,6 +0,0 @@
-#
-from .task import Task;
-from .datasets import ShapeNet;
-class RealTask(Task):
-    def __init__(self):
-        super(RealTask,self).__init__();
