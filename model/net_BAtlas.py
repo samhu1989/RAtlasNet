9a7f1330f887b8f054414ebf1867ba3d59544f61
Sam Hu <hsy19891228@yeah.net>
2019-02-02 16:54:37 +0800

    update code

--- model/net_BAtlas.py
+++ model/net_BAtlas.py
@@ -2,41 +2,41 @@ from .net_BlendedAtlasNet import *;
 
 class PointMLP(nn.Module):
     def __init__(self, bottleneck_size=2500,odim=3,bn=True):
-        self.bottleneck_size = bottleneck_size;
-        super(PointMLP, self).__init__();
-        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//4,1);
-        self.conv2 = torch.nn.Conv1d(self.bottleneck_size//4,self.bottleneck_size//16,1);
-        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//16,self.bottleneck_size//32,1);
-        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//32,odim,1);
-        self.bn = bn;
-        self.act = nn.Softmax(dim=1);
+        self.bottleneck_size = bottleneck_size
+        super(PointMLP, self).__init__()
+        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//4,1)
+        self.conv2 = torch.nn.Conv1d(self.bottleneck_size//4,self.bottleneck_size//16,1)
+        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//16,self.bottleneck_size//32,1)
+        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//32,odim,1)
+        self.bn = bn
+        self.act = nn.Softmax(dim=1)
         if self.bn:
-            self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size//4);
-            self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//16);
-            self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//32);
+            self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size//4)
+            self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//16)
+            self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//32)
         
     def forward(self, x):
         if self.bn:
-            x = F.relu(self.bn1(self.conv1(x)));
-            x = F.relu(self.bn2(self.conv2(x)));
-            x = F.relu(self.bn3(self.conv3(x)));
+            x = F.relu(self.bn1(self.conv1(x)))
+            x = F.relu(self.bn2(self.conv2(x)))
+            x = F.relu(self.bn3(self.conv3(x)))
             x = self.act(self.conv4(x))
         else:
-            x = F.relu(self.conv1(x));
-            x = F.relu(self.conv2(x));
-            x = F.relu(self.conv3(x));
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = F.relu(self.conv3(x))
             x = self.act(self.conv4(x))
-        return x;
+        return x
 
 class BAtlas(nn.Module):
     def __init__(self,*args,**kwargs):
-        super(BAtlas, self).__init__();
-        self.pts_num = kwargs['pts_num'];
-        self.bottleneck_size = 1024;
-        self.grid_num = kwargs['grid_num'];
-        self.grid_dim = kwargs['grid_dim'];
-        self.mode = kwargs['mode'];
-        self.bn = True;
+        super(BAtlas, self).__init__()
+        self.pts_num = kwargs['pts_num']
+        self.bottleneck_size = 1024
+        self.grid_num = kwargs['grid_num']
+        self.grid_dim = kwargs['grid_dim']
+        self.mode = kwargs['mode']
+        self.bn = True
         if self.mode == 'SVR':
             self.encoder = resnet.resnet18(pretrained=False,num_classes=1024);
         elif self.mode == 'AE':
@@ -45,79 +45,79 @@ class BAtlas(nn.Module):
                 nn.Linear(1024, self.bottleneck_size),
                 nn.BatchNorm1d(self.bottleneck_size),
                 nn.ReLU()
-                );
+                )
         elif self.mode == 'OPT':
-            self.encoder = OptEncoder(self.bottleneck_size);
+            self.encoder = OptEncoder(self.bottleneck_size)
             self.bn = False
         else:
             assert False,'unkown mode of BlendedAtlasNet'
         self.wdecoder = PointMLP(bottleneck_size=self.grid_dim+self.bottleneck_size,odim=self.grid_num,bn=self.bn);
         self.decoder = nn.ModuleList([PointMLP(bottleneck_size=self.grid_dim+self.bottleneck_size,odim=3,bn=self.bn) for i in range(0,self.grid_num)]);
         self.inv_decoder = PointGenCon(bottleneck_size=3+self.bottleneck_size,odim=3,bn=self.bn);
-        self._init_layers();
+        self._init_layers()
 
     def forward(self,*input):
-        x = input[0];
+        x = input[0]
         if x.dim() == 4:
-            x = x[:,:3,:,:].contiguous();
-        f = self.encoder(x);
-        outs = [];
-        grid = None;
+            x = x[:,:3,:,:].contiguous()
+        f = self.encoder(x)
+        outs = []
+        grid = None
         if len(input) > 1:
-            grid = input[1];
+            grid = input[1]
         else:
-            grid = self.rand_grid(f);
-        inv_e = 0.0;
+            grid = self.rand_grid(f)
+        inv_e = 0.0
         expf = f.unsqueeze(2).expand(f.size(0),f.size(1),grid.size(2)).contiguous();
-        w = torch.cat((grid,expf),1).contiguous();
-        w = self.wdecoder(w);
-        w = w.view(w.size(0),w.size(1),1,w.size(2));
-        yo = None;
+        w = torch.cat((grid,expf),1).contiguous()
+        w = self.wdecoder(w)
+        w = w.view(w.size(0),w.size(1),1,w.size(2))
+        yo = None
         for i in range(0,self.grid_num):
-            y = torch.cat((grid,expf),1).contiguous();
-            y = self.decoder[i](y);
-            y = y*w[:,i,:,:];
+            y = torch.cat((grid,expf),1).contiguous()
+            y = self.decoder[i](y)
+            y = y*w[:,i,:,:]
             if yo is None:
-                yo = y;
+                yo = y
             else:
-                yo += y;
-        invo = torch.cat((yo,expf),1).contiguous();
-        invo = self.inv_decoder(invo);
+                yo += y
+        invo = torch.cat((yo,expf),1).contiguous()
+        invo = self.inv_decoder(invo)
         #transpose from (bn,3,#points) to (bn,3,#points)
-        yo = yo.transpose(2,1).contiguous();
-        invo = invo.transpose(2,1).contiguous();
-        grid = grid.transpose(2,1).contiguous();
-        out = {};
-        out['y'] = yo;
-        out['inv_x'] = invo;
-        out['grid_x'] = grid;
-        out['blend_w'] = w;
-        return out;
+        yo = yo.transpose(2,1).contiguous()
+        invo = invo.transpose(2,1).contiguous()
+        grid = grid.transpose(2,1).contiguous()
+        out = {}
+        out['y'] = yo
+        out['inv_x'] = invo
+        out['grid_x'] = grid
+        out['blend_w'] = w
+        return out
     
     def rand_grid(self,x):
         rand_grid = torch.FloatTensor(x.size(0),self.grid_dim,self.pts_num);
         if self.grid_dim == 3:
-            rand_grid.normal_(0.0,1.0);
-            rand_grid += 1e-9;
+            rand_grid.normal_(0.0,1.0)
+            rand_grid += 1e-9
             rand_grid = rand_grid / torch.norm(rand_grid,p=2.0,dim=1,keepdim=True);
         else:
-            rand_grid.uniform_(0.0,1.0);
+            rand_grid.uniform_(0.0,1.0)
         if isinstance(x,Variable):
-            rand_grid = Variable(rand_grid);
+            rand_grid = Variable(rand_grid)
         if x.is_cuda:
-            rand_grid = rand_grid.cuda();
-        return rand_grid;
+            rand_grid = rand_grid.cuda()
+        return rand_grid
     
     def _init_layers(self):
         for m in self.modules():
             if isinstance(m,nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels;
-                m.weight.data.normal_(0,math.sqrt(2./n));
+                m.weight.data.normal_(0,math.sqrt(2./n))
             elif isinstance(m,nn.Conv1d):
-                m.weight.data.normal_(0.0,0.02);
+                m.weight.data.normal_(0.0,0.02)
             elif isinstance(m,nn.BatchNorm1d):
-                m.weight.data.normal_(1.0,0.02);
+                m.weight.data.normal_(1.0,0.02)
                 m.bias.data.fill_(0)
             elif isinstance(m,nn.BatchNorm2d):
-                m.weight.data.fill_(1);
-                m.bias.data.zero_();
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
--- model/net_BlendedAtlasNet.py
+++ model/net_BlendedAtlasNet.py
@@ -1,20 +1,20 @@
-import sys;
-import os;
-sys.path.append(os.path.dirname(__file__));
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
 import torch
 import torch.nn as nn
 import torch.nn.parallel
 import torch.backends.cudnn as cudnn
-import torch.optim as optim;
+import torch.optim as optim
 from torch.nn.parameter import Parameter
 import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 from torch.autograd import Variable
 import numpy as np
-import torch.nn.functional as F;
-import resnet;
-import math;
+import torch.nn.functional as F
+import resnet
+import math
   
 #atlasnet
 class STN3d(nn.Module):
@@ -92,174 +92,161 @@ class PointNetfeat(nn.Module):
 
 class PointGenCon(nn.Module):
     def __init__(self, bottleneck_size=2500,odim=3,bn=True):
-        self.bottleneck_size = bottleneck_size;
-        super(PointGenCon, self).__init__();
-        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size,1);
-        self.conv2 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//2,1);
-        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//2,self.bottleneck_size//4,1);
-        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//4,odim,1);
-        self.bn = bn;
-        self.th = nn.Tanh();
+        self.bottleneck_size = bottleneck_size
+        super(PointGenCon, self).__init__()
+        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size,1)
+        self.conv2 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//2,1
+        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//2,self.bottleneck_size//4,1)
+        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//4,odim,1)
+        self.bn = bn
+        self.th = nn.Tanh()
         if self.bn:
-            self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size);
-            self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2);
-            self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4);
+            self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size)
+            self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2)
+            self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4)
         
     def forward(self, x):
         # print(x.size());
         if self.bn:
-            x = F.relu(self.bn1(self.conv1(x)));
-            x = F.relu(self.bn2(self.conv2(x)));
-            x = F.relu(self.bn3(self.conv3(x)));
+            x = F.relu(self.bn1(self.conv1(x)))
+            x = F.relu(self.bn2(self.conv2(x)))
+            x = F.relu(self.bn3(self.conv3(x)))
             x = self.th(self.conv4(x))
         else:
-            x = F.relu(self.conv1(x));
-            x = F.relu(self.conv2(x));
-            x = F.relu(self.conv3(x));
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = F.relu(self.conv3(x))
             x = self.th(self.conv4(x))
-        return x;
+        return x
         
 class WGenCon(nn.Module):
     def __init__(self, bottleneck_size=2500,odim=10,bn=True):
-        self.bottleneck_size = bottleneck_size;
-        super(WGenCon, self).__init__();
-        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size,1);
-        self.conv2 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//2,1);
-        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//2,self.bottleneck_size//4,1);
-        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//4,odim,1);
-        self.bn = bn;
-        self.act = nn.Softmax(dim=1);
+        self.bottleneck_size = bottleneck_size
+        super(WGenCon, self).__init__()
+        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size,1)
+        self.conv2 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//2,1)
+        self.conv3 = torch.nn.Conv1d(self.bottleneck_size//2,self.bottleneck_size//4,1)
+        self.conv4 = torch.nn.Conv1d(self.bottleneck_size//4,odim,1)
+        self.bn = bn
+        self.act = nn.Softmax(dim=1)
         if self.bn:
-            self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size);
-            self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2);
-            self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4);
+            self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size)
+            self.bn2 = torch.nn.BatchNorm1d(self.bottleneck_size//2)
+            self.bn3 = torch.nn.BatchNorm1d(self.bottleneck_size//4)
         
     def forward(self, x):
         if self.bn:
-            x = F.relu(self.bn1(self.conv1(x)));
-            x = F.relu(self.bn2(self.conv2(x)));
-            x = F.relu(self.bn3(self.conv3(x)));
+            x = F.relu(self.bn1(self.conv1(x)))
+            x = F.relu(self.bn2(self.conv2(x)))
+            x = F.relu(self.bn3(self.conv3(x)))
             x = self.act(self.conv4(x))
         else:
-            x = F.relu(self.conv1(x));
-            x = F.relu(self.conv2(x));
-            x = F.relu(self.conv3(x));
+            x = F.relu(self.conv1(x))
+            x = F.relu(self.conv2(x))
+            x = F.relu(self.conv3(x))
             x = self.act(self.conv4(x))
-        return x;
+        return x
     
 class OptEncoder(nn.Module):
     def __init__(self,bottleneck_size=1024):
-        self.bottleneck_size = bottleneck_size;
-        super(OptEncoder,self).__init__();
-        self.f = Parameter(torch.zeros(1,self.bottleneck_size));
-        self.f.data.normal_(0,math.sqrt(2./float(self.bottleneck_size)));
+        self.bottleneck_size = bottleneck_size
+        super(OptEncoder,self).__init__()
+        self.f = Parameter(torch.zeros(1,self.bottleneck_size))
+        self.f.data.normal_(0,math.sqrt(2./float(self.bottleneck_size)))
         
     def forward(self,x):
-        return self.f;
-''' 
-class OptDecoder(nn.Module):
-    def __init__(self,bottleneck_size=1024):
-        self.bottleneck_size = bottleneck_size;
-        super(OptDecoder,self).__init__();
-        self.conv1 = torch.nn.Conv1d(self.bottleneck_size,self.bottleneck_size//2,1);
-        self.conv2 = torch.nn.Conv1d(self.bottleneck_size,3,1);
-        self.bn1 = torch.nn.BatchNorm1d(self.bottleneck_size//2);
-        self.th = nn.Tanh();
-    def forward(self,x):
-        x = F.relu(self.bn1(self.conv1(x)));
-        return self.th(self.conv2(x));
-'''
+        return self.f
         
 class BlendedAtlasNet(nn.Module):
     def __init__(self,*args,**kwargs):
-        super(BlendedAtlasNet, self).__init__();
-        self.pts_num = kwargs['pts_num'];
-        self.bottleneck_size = 1024;
-        self.grid_num = kwargs['grid_num'];
-        self.grid_dim = kwargs['grid_dim'];
-        self.mode = kwargs['mode'];
-        self.bn = True;
+        super(BlendedAtlasNet, self).__init__()
+        self.pts_num = kwargs['pts_num']
+        self.bottleneck_size = 1024
+        self.grid_num = kwargs['grid_num']
+        self.grid_dim = kwargs['grid_dim']
+        self.mode = kwargs['mode']
+        self.bn = True
         if self.mode == 'SVR':
-            self.encoder = resnet.resnet18(pretrained=False,num_classes=1024);
+            self.encoder = resnet.resnet18(pretrained=False,num_classes=1024)
         elif self.mode == 'AE':
             self.encoder = nn.Sequential(
                 PointNetfeat(self.pts_num, global_feat=True, trans = False),
                 nn.Linear(1024, self.bottleneck_size),
                 nn.BatchNorm1d(self.bottleneck_size),
                 nn.ReLU()
-                );
+                )
         elif self.mode == 'OPT':
-            self.encoder = OptEncoder(self.bottleneck_size);
+            self.encoder = OptEncoder(self.bottleneck_size)
             self.bn = False
         else:
             assert False,'unkown mode of BlendedAtlasNet'
         self.wdecoder = WGenCon(bottleneck_size=self.grid_dim+self.bottleneck_size,odim=self.grid_num,bn=self.bn);
         self.decoder = nn.ModuleList([PointGenCon(bottleneck_size=self.grid_dim+self.bottleneck_size,bn=self.bn) for i in range(0,self.grid_num)]);
-        self.inv_decoder = PointGenCon(bottleneck_size=3+self.bottleneck_size,bn=self.bn);
-        self._init_layers();
+        self.inv_decoder = PointGenCon(bottleneck_size=3+self.bottleneck_size,bn=self.bn)
+        self._init_layers()
 
     def forward(self,*input):
-        x = input[0];
+        x = input[0]
         if x.dim() == 4:
-            x = x[:,:3,:,:].contiguous();
-        f = self.encoder(x);
-        outs = [];
-        grid = None;
+            x = x[:,:3,:,:].contiguous()
+        f = self.encoder(x)
+        outs = []
+        grid = None
         if len(input) > 1:
-            grid = input[1];
+            grid = input[1]
         else:
-            grid = self.rand_grid(f);
-        inv_e = 0.0;
-        expf = f.unsqueeze(2).expand(f.size(0),f.size(1),grid.size(2)).contiguous();
-        w = torch.cat((grid,expf),1).contiguous();
-        w = self.wdecoder(w);
-        w = w.view(w.size(0),w.size(1),1,w.size(2));
-        yo = None;
+            grid = self.rand_grid(f)
+        inv_e = 0.0
+        expf = f.unsqueeze(2).expand(f.size(0),f.size(1),grid.size(2)).contiguous()
+        w = torch.cat((grid,expf),1).contiguous()
+        w = self.wdecoder(w)
+        w = w.view(w.size(0),w.size(1),1,w.size(2))
+        yo = None
         for i in range(0,self.grid_num):
-            y = torch.cat((grid,expf),1).contiguous();
-            y = self.decoder[i](y);
-            y = y*w[:,i,:,:];
+            y = torch.cat((grid,expf),1).contiguous()
+            y = self.decoder[i](y)
+            y = y*w[:,i,:,:]
             if yo is None:
-                yo = y;
+                yo = y
             else:
-                yo += y;
-        invo = torch.cat((yo,expf),1).contiguous();
-        invo = self.inv_decoder(invo);
+                yo += y
+        invo = torch.cat((yo,expf),1).contiguous()
+        invo = self.inv_decoder(invo)
         #transpose from (bn,3,#points) to (bn,3,#points)
-        yo = yo.transpose(2,1).contiguous();
-        invo = invo.transpose(2,1).contiguous();
-        grid = grid.transpose(2,1).contiguous();
-        out = {};
-        out['y'] = yo;
-        out['inv_x'] = invo;
-        out['grid_x'] = grid;
-        out['blend_w'] = w;
-        return out;
+        yo = yo.transpose(2,1).contiguous()
+        invo = invo.transpose(2,1).contiguous()
+        grid = grid.transpose(2,1).contiguous()
+        out = {}
+        out['y'] = yo
+        out['inv_x'] = invo
+        out['grid_x'] = grid
+        out['blend_w'] = w
+        return out
     
     def rand_grid(self,x):
-        rand_grid = torch.FloatTensor(x.size(0),self.grid_dim,self.pts_num);
+        rand_grid = torch.FloatTensor(x.size(0),self.grid_dim,self.pts_num)
         if self.grid_dim == 3:
-            rand_grid.normal_(0.0,1.0);
-            rand_grid += 1e-9;
-            rand_grid = rand_grid / torch.norm(rand_grid,p=2.0,dim=1,keepdim=True);
+            rand_grid.normal_(0.0,1.0)
+            rand_grid += 1e-9
+            rand_grid = rand_grid / torch.norm(rand_grid,p=2.0,dim=1,keepdim=True)
         else:
-            rand_grid.uniform_(0.0,1.0);
+            rand_grid.uniform_(0.0,1.0)
         if isinstance(x,Variable):
-            rand_grid = Variable(rand_grid);
+            rand_grid = Variable(rand_grid)
         if x.is_cuda:
-            rand_grid = rand_grid.cuda();
-        return rand_grid;
+            rand_grid = rand_grid.cuda()
+        return rand_grid
     
     def _init_layers(self):
         for m in self.modules():
             if isinstance(m,nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels;
-                m.weight.data.normal_(0,math.sqrt(2./n));
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0,math.sqrt(2./n))
             elif isinstance(m,nn.Conv1d):
-                m.weight.data.normal_(0.0,0.02);
+                m.weight.data.normal_(0.0,0.02)
             elif isinstance(m,nn.BatchNorm1d):
-                m.weight.data.normal_(1.0,0.02);
+                m.weight.data.normal_(1.0,0.02)
                 m.bias.data.fill_(0)
             elif isinstance(m,nn.BatchNorm2d):
-                m.weight.data.fill_(1);
-                m.bias.data.zero_();
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
